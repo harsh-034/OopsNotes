7941383c79d9e8f3c7dd310b5d60b6ba55ab8b8a
harsh-034 <harshgupta00034@gmail.com>
2026-07-17 20:15:49 +0530

    add new concept of Inheritance

--- program/src/Inheritance/Singal.java
+++ program/src/Inheritance/Singal.java
@@ -1,4 +1,19 @@
 package Inheritance;
 
-public class Singal {
+ class studentinfo {
+    public String name;
+    public int age;
+    public int stenderd;
+    public void eat(){
+        System.out.println("Student Eat Food.");
+    }
 }
+public class Singal extends studentinfo{
+    public void display(){
+        System.out.println("Student Name " + name);
+        System.out.println("Student Age :" + age);
+        System.out.println("Student Class :" + stenderd+"th");
+    }
+}
+
+
