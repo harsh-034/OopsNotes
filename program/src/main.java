69d95d419d103512b47f4eb5e6653f1b49719df6
harsh-034 <harshgupta00034@gmail.com>
2026-07-17 20:17:33 +0530

    update main

--- program/src/main.java
+++ program/src/main.java
@@ -1,5 +1,8 @@
 import Constructor.Default;
 import Constructor.Paramiterized;
+import Incapsulation.incapsulationone;
+import Inheritance.Singal;
+
 import basic_class_object.Basic_1;
 
 import java.util.Scanner;
@@ -16,19 +19,45 @@ public class main {
         b.age = sc.nextInt();
         System.out.println("Emter your class :");
         b.stenderd = sc.nextInt();
-        // b.display();
+         b.display();
 
         //====================================
+        System.out.println("=============Constructor==================");
         // default constructer
         Default d = new Default();
         //No argument constructer;
         //without maintion any argument only use constercre name for method like constructer()
         // paramiterize constructer
+
+
         Paramiterized p1 = new  Paramiterized(b.age);
         Paramiterized p2 = new  Paramiterized(b.name,b.age);
         Paramiterized p3 = new  Paramiterized(b.name);
-        //
+        //===========================================================
+        //Encapsulation
+        System.out.println("=============Encapsulation==================");
 
+        incapsulationone i = new incapsulationone();
+
+        i.setName(b.name);
+        i.setAge(b.age);
+        i.setStendrd(b.stenderd);
+        i.setGender("Male");
+
+        System.out.println("Student Name : " + i.getName());
+        System.out.println("Student Age :"+i.getAge());
+        System.out.println("Student Class :"+i.getStendrd());
+        System.out.println("Student Gender : "+i.getGender());
+
+        //======================================================
+        System.out.println("===============Inheritance=====================");
+
+        Singal stu1 = new Singal();
+        stu1.name = b.name;
+        stu1.age = b.age;
+        stu1.stenderd = b.stenderd;
+        stu1.display();
+        stu1.eat();
 
     }
 }
